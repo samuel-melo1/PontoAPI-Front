870b644cd83b94dd2ec1e90cdc8ebf7507007b24
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-11-06 23:56:37 -0300

    implemented model users

---
+++ src/app/models/users.ts
@@ -0,0 +1,11 @@
+export interface Users{
+    id?: any;
+    name: string; 
+    cpf: string; 
+    email: string;
+    password: string; 
+    telefone: string; 
+    cargo: string; 
+    departamento: string; 
+    permissions: string[]; 
+}
